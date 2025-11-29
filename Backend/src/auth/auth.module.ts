c3170984aac9c93b7402899ddbcb45d49ce569a0
FacuGallardo <gashardo23@gmail.com>
2025-11-29 18:19:19 -0300

    Update auth.module.ts

--- Backend/src/auth/auth.module.ts
+++ Backend/src/auth/auth.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { ReferenteModule } from '../referente/referente.module'; // <--- Importar archivo
+import { ReferenteModule } from '../referente/referente.module';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    ReferenteModule, // <--- AGREGAR ESTO EN IMPORTS
+    ReferenteModule,
+    PassportModule,
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60m' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
